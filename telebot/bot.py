3940ac74132f00415d92c1329bb9d8092849441b
alice <reitochka@gmail.com>
2017-04-28 21:11:30 +0300

    some string manipulations

--- telebot/bot.py
+++ telebot/bot.py
@@ -3,8 +3,9 @@ import config
 import telebot
 import tensorflow as tf
 import sys, os  
+import random
 sys.path.append("../tensorflow_chatbot")
-import execute
+#import execute
 
 '''
 Init seq2seq model
@@ -21,6 +22,10 @@ Init Telegram Bot with token from @BotFather
 
 bot = telebot.TeleBot(config.token)
 
+dissatisfacted_stickers = ['CAADAgADegcAAlOx9wPdYZQdsjeJ1QI', 'CAADAgADfAcAAlOx9wPFib3kWlKcTgI', 'CAADAgADZAcAAlOx9wMOPOsobSvsAwI',
+                            'CAADAgADcAcAAlOx9wPvD5GVsIF2awI', 'CAADAgADZgcAAlOx9wP_l3UsB3QgqgI', 'CAADAgADawADCcWmA1VTC_qsRfCDAg', 'CAADAgADcwADCcWmAyzf617bF45OAg',
+                            'CAADAgADZAADCcWmAzEQWoj97GwoAg', 'CAADAgADVgADCcWmA3t13Jtwo52vAg', 'CAADAgADdAADCcWmA7q-xty48LXYAg', 'CAADAgADUwADCcWmA2mcT2RpHaCuAg']
+
 @bot.message_handler(commands=['start', 'help'])
 def handle_start_help(message):
     print('['+ message.from_user.username + '] : '+ message.text)
@@ -31,30 +36,34 @@ def handle_start_help(message):
 
 @bot.message_handler(content_types=['sticker'])
 def repeat_sticker(message): # Название функции не играет никакой роли, в принципе
-    user = message.date
     if message.sticker:
     	print('['+ message.from_user.username + '] : '+ message.sticker.file_id)
     	bot.send_message(message.chat.id, 'Получи назад АХАХАХАХ')
     	bot.send_sticker(message.chat.id, message.sticker.file_id)
 
-@bot.message_handler(content_types=["text"])
+'''@bot.message_handler(content_types=["text"])
 def say_I_know_your_location(message): # Название функции не играет никакой роли, в принципе
-    user = message.date
     if message.text:
     	print('['+ message.from_user.username + '] : '+ message.text)
-    	bot.send_message(message.chat.id, 'Я знаю где ты живешь, '+ message.from_user.first_name)
+    	bot.send_message(message.chat.id, 'Я знаю где ты живешь, '+ message.from_user.first_name)'''
 
 # answer message with Adventure Time 
 @bot.message_handler(content_types=["text"])
 def answer_adventure_time(message): # Название функции не играет никакой роли, в принципе
-    user = message.date
     if message.text:
         print('['+ message.from_user.username + '] : '+ message.text)
-        mess = ''
-        #mess = execute.decode_line(sess, model, enc_vocab, rev_dec_vocab, message.text)
+        mess = 'hello, dear user_name, i like you user_name'
+        question = message.text.lower()
+        #mess = execute.decode_line(sess, model, enc_vocab, rev_dec_vocab, question)
+        if '_UNK' in mess:
+            print(mess)
+            mess = 'I don\'t understand you'
+            bot.send_sticker(message.chat.id, dissatisfacted_stickers[random.randint(0, 10)])        
         if 'user_name' in mess:
-            print('need to replace name')
-        bot.send_message(message.chat.id, 'Я знаю где ты живешь, '+ mess)
+            mess = mess.replace('user_name', message.from_user.first_name)
+        mess = mess[0].upper()+mess[1:]
+        mess = mess.replace(' i ', ' I ')
+        bot.send_message(message.chat.id, mess)
 
 
 if __name__ == '__main__':
