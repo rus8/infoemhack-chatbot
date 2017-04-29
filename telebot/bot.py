130f9ab95b05938928e5acf0716b8e2148179690
alice <reitochka@gmail.com>
2017-04-29 07:15:03 +0300

    more postprocessing

--- telebot/bot.py
+++ telebot/bot.py
@@ -5,7 +5,7 @@ import tensorflow as tf
 import sys, os  
 import random
 sys.path.append("../tf_chatbot")
-import execute
+#import execute
 
 '''
 Init seq2seq model
@@ -13,8 +13,8 @@ Init seq2seq model
     2. Create decode_line function that takes message as input
 '''
 
-sess = tf.Session()
-sess, model, enc_vocab, rev_dec_vocab = execute.init_session(sess, conf='../tf_chatbot/seq2seq.ini')
+#sess = tf.Session()
+#sess, model, enc_vocab, rev_dec_vocab = execute.init_session(sess, conf='../tf_chatbot/seq2seq.ini')
 
 '''
 Init Telegram Bot with token from @BotFather
@@ -58,6 +58,37 @@ def preprocessing(strr):
             strr = strr.replace(key, value)
         elif key[0]==' ' and strr.find(key[1:])==0:
             strr = strr.replace(key[1:], value[1:])
+    print(strr)
+    return strr
+
+def postprocessing(strr, message):
+    print('came to post:'+strr)
+    #if 'user_name' in strr:
+    #    strr = strr.replace('user_name', message.from_user.first_name) # замена собственных имен на твое имя
+    symbols = ['.', '?', '!',',']
+    adventure_heroes = [' fin ', ' jake ', ' bubblegum ', ' bmo ']
+    b = []
+    a = strr.strip().split()
+    '''if len(a) == 1:
+        b = a
+    else:'''
+    for i in range(0, len(a)):
+        if (not (' '+a[i] in b) and not (a[i] in symbols)):
+            if ' '+a[i]+' ' in adventure_heroes:
+                b.append(' '+ message.from_user.first_name)
+            else:
+                b.append(' ' + a[i])
+        elif (a[i] in symbols and  not b[len(b)-1] in symbols):
+            b.append(a[i])
+
+    strr = ''.join(b)
+    strr = strr[1].upper() + strr[2:]
+    strr = strr.replace(' i ', ' I ')
+    for i in range(0, len(strr)):
+        if strr[i] in symbols[:3]:
+            if i+2 < len(strr)-1:
+                strr = strr[:i+2] + strr[i+2].upper() + strr[i+3:]
+
     return strr
 
 # answer message with Adventure Time 
@@ -65,17 +96,14 @@ def preprocessing(strr):
 def answer_adventure_time(message): # Название функции не играет никакой роли, в принципе
     if message.text:
         print('['+ message.from_user.username + '] : '+ message.text)
-        mess = 'hello, dear user_name, i like you user_name'
-        question = preprocessing(message.text)      
-        mess = execute.decode_line(sess, model, enc_vocab, rev_dec_vocab, question)
-        if '_UNK' in mess:
-            mess = 'I don\'t understand you'
-            bot.send_sticker(message.chat.id, dissatisfacted_stickers[random.randint(0, 10)])        
-        if 'user_name' in mess:
-            mess = mess.replace('user_name', message.from_user.first_name)
-        mess = mess[0].upper()+mess[1:]
-        mess = mess.replace(' i ', ' I ')
-        bot.send_message(message.chat.id, mess)
+        answ = preprocessing(message.text)
+        #answ = execute.decode_line(sess, model, enc_vocab, rev_dec_vocab, question)
+        if '_UNK' in answ:
+            answ = "I don't understand you"
+            bot.send_sticker(message.chat.id, dissatisfacted_stickers[random.randint(0, 10)])
+        else:
+            answ = postprocessing(answ, message)
+        bot.send_message(message.chat.id, answ)
 
 
 if __name__ == '__main__':
